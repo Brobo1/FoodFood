68010953a788dc2b26287f42c6e76fe4224caad2
Arne Ngo <arnengo2812@hotmail.com>
2024-03-18 13:53:40 +0100

    finished payment

--- FoodFood/Controller/PaymentController.cs
+++ FoodFood/Controller/PaymentController.cs
@@ -2,16 +2,35 @@
 using FoodFood.Data;
 using FoodFood.Data.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FoodFood.Controller
 {
     public class PaymentController : ControllerBase
     {
-        private readonly FoodFoodContext _context;
+        private readonly FoodFoodContext _db;
 
-        public PaymentController(FoodFoodContext context)
+        public PaymentController(FoodFoodContext db)
         {
-            _context = context;
+            _db = db;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> Get(int id)
+        {
+            var payment = await _db.Payment.FindAsync(id);
+            if(payment == null)
+            {
+                return NotFound();
+            }
+            return Ok(payment);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            var payments = await _db.Payment.ToListAsync();
+            return Ok(payments);
         }
 
         [HttpPost]
@@ -24,16 +43,38 @@ namespace FoodFood.Controller
                 ExpirationDate = paymentPost.expirationDate,
                 CardHolderName = paymentPost.cardholderName!
             };
-            _context.Payment.Add(payment);
-            await _context.SaveChangesAsync();
+            _db.Payment.Add(payment);
+            await _db.SaveChangesAsync();
             return Ok(payment);
         }
 
-        //[HttpPut]
-        //public async Task<ActionResult> Update(PaymentModel paymentPut)
-        //{
-        //    var paymentToPut = await _context.Payment.FindAsync(paymentPut.Id);
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(int id, PaymentModel paymentPut)
+        {
+            var paymentToPut = await _db.Payment.FindAsync(id);
+            if(paymentToPut == null) 
+            {
+                return NotFound();
+            }
+            paymentToPut.Type = paymentPut.Type!;
+            paymentToPut.CardNumber = paymentPut.cardNumber;
+            paymentToPut.ExpirationDate = paymentPut.expirationDate;
+            paymentToPut.CardHolderName = paymentPut.cardholderName!;
 
-        //}
+            await _db.SaveChangesAsync();
+            return Ok(paymentToPut);
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id) 
+        {
+            var paymentToDelete = await _db.Payment.FindAsync(id);
+            if(paymentToDelete == null)
+            {
+                return NotFound();
+            }
+            _db.Payment.Remove(paymentToDelete);
+            await _db.SaveChangesAsync();
+            return Ok();
+        }
     }
 }
