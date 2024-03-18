f3f00796f9f262d15847498bfcbc857bd29aca66
AgustinaScapusio <agustina.scapusio.99@gmail.com>
2024-03-18 10:07:57 +0100

    Added PasswordHasher

---
+++ FoodFood/Security/PasswordHasher.cs
@@ -0,0 +1,37 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace FoodFood.Security { 
+
+public static class PasswordHasher
+{
+
+    public static string HashPassword(string unhashedPasswordAndSalt)
+    {
+        // Create an instance of SHA-256
+        using (var sha256 = SHA256.Create())
+        {
+            // Convert the password to a byte array
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(unhashedPasswordAndSalt);
+
+            // Compute the hash
+            byte[] hashedBytes = sha256.ComputeHash(passwordBytes);
+
+            // Convert the hashed bytes to a base64-encoded string for storage
+            string hashedPassword = Convert.ToBase64String(hashedBytes);
+            return hashedPassword;
+        }
+    }
+    public static string GenerateSalt()
+    {
+        byte[] saltBytes = new byte[16];
+        using (var rng = RandomNumberGenerator.Create())
+        {
+            rng.GetBytes(saltBytes);
+        }
+
+        string salt = Convert.ToBase64String(saltBytes);
+        return salt;
+    }
+}
+}
