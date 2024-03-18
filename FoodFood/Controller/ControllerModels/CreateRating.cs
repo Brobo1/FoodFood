7272121334d7123267e73af81d20fe2855b20b63
brobo1 <armanahz95@gmail.com>
2024-03-18 15:42:52 +0100

    Remove initial migration files
    The committed changes include the removal of initial migration files related to the FoodFood project. These files seem to contain migration setup for various components including meals, restaurants, and users among others. This also implies that the database structure, as defined by these migrations, has been eliminated.

--- FoodFood/Controller/ControllerModels/CreateRating.cs
+++ FoodFood/Controller/ControllerModels/CreateRating.cs
@@ -1,9 +1,9 @@
-﻿namespace FoodFood.Controller.ControllerModels
-{
-    public class CreateRating
-    {
-        public string Review { get; set; } = string.Empty;
-        public int UserId { get; set; }
-        public int Stars { get; set; }
-    }
+﻿namespace FoodFood.Controller.ControllerModels {
+	public class CreateRating {
+		public string Review { get; set; } = string.Empty;
+		public int    UserId { get; set; }
+		public int    Stars  { get; set; }
+	}
+
+
 }
--- FoodFood/Data/Models/Rating.cs
+++ FoodFood/Data/Models/Rating.cs
@@ -1,10 +1,10 @@
 ﻿namespace FoodFood.Data.Models;
 
 public class Rating {
-	public int    Id     { get; set; }
-	public string Review { get; set; } = string.Empty;
-	public int   UserId { get; set; }
-	public int    Stars  { get; set; }
-	public User?  User { get; set; }
+	public int                            Id               { get; set; }
+	public string                         Review           { get; set; } = string.Empty;
+	public int                            UserId           { get; set; }
+	public int                            Stars            { get; set; }
+	public User?                          User             { get; set; }
 	public ICollection<RestaurantRating>? RestaurantRating { get; set; }
 }
