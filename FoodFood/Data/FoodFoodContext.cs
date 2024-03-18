73cb682e0dcf3fb5ba2538a31c8f843c49d324c6
Arne Ngo <arnengo2812@hotmail.com>
2024-03-18 10:11:32 +0100

    dbContext completed

--- FoodFood/Data/FoodFoodContext.cs
+++ FoodFood/Data/FoodFoodContext.cs
@@ -1,5 +1,4 @@
-﻿using FoodFood.Controller;
-using FoodFood.Data.Models;
+﻿using FoodFood.Data.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace FoodFood.Data
@@ -13,10 +12,14 @@ namespace FoodFood.Data
 
         public DbSet<Models.Favorite> Favorite { get; set; }
         public DbSet<Models.Meal> Meal { get; set; }
-        public DbSet<Models.Meal_Restaurant> Meal_Resturant { get; set; }
+        public DbSet<Models.MealOrder> MealOrder { get; set; }
+        public DbSet<Models.MealRestaurant> MealResturant { get; set; }
+        public DbSet<Models.Order> Order { get; set; }
         public DbSet<Models.Payment> Payment { get; set; }
-
-
-
+        public DbSet<Models.Personalia> Personalia { get; set; }
+        public DbSet<Models.Rating> Rating { get; set; }
+        public DbSet<Models.Restaurant> Restaurants { get; set; }
+        public DbSet<Models.RestaurantRating> RestaurantRatings { get; set; }
+        public DbSet<Models.User> User { get; set; }
     }
 }
