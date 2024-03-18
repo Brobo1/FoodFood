4693fde380bc21b949b0adf796ecd92e529d3610
AgustinaScapusio <agustina.scapusio.99@gmail.com>
2024-03-18 09:36:29 +0100

    Added Meal, Meal_Restaurant,Restaurant and Restaurant_Rating classes

---
+++ FoodFood/Data/Models/Meal.cs
@@ -0,0 +1,16 @@
+﻿namespace FoodFood.Data.Models
+{
+    public class Meal
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public string MealImage { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string Allergens { get; set; } = string.Empty;
+        public ICollection<Meal_Restaurant> Meal_Restaurants { get; set; } = new List<Meal_Restaurant>();
+
+        
+    }
+}
---
+++ FoodFood/Data/Models/Restaurant.cs
@@ -0,0 +1,18 @@
+﻿namespace FoodFood.Data.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime OpeningTime { get; set; } = DateTime.Now;
+        public DateTime ClosingTime { get; set; } = DateTime.Now;
+        public bool IsOpen { get; set; }
+        public string Image { get; set; } = string.Empty;
+        public ICollection<Meal_Restaurant> Meal_Restaurants { get; set; } = new List<Meal_Restaurant>();
+        public ICollection<Restaurant_Rating> Restaurant_Ratings { get; set; } = new List<Restaurant_Rating>();
+    }
+
+}
