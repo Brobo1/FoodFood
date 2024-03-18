5b5e3ae9510537e6dc214a1a3f1b213a1620f307
AgustinaScapusio <agustina.scapusio.99@gmail.com>
2024-03-18 14:02:26 +0100

    Added RatingController, FavoriteController, CreateFavorite and CreateRating

---
+++ FoodFood/Controller/ControllerModels/CreateRating.cs
@@ -0,0 +1,9 @@
+﻿namespace FoodFood.Controller.ControllerModels
+{
+    public class CreateRating
+    {
+        public string Review { get; set; } = string.Empty;
+        public int UserId { get; set; }
+        public int Stars { get; set; }
+    }
+}
