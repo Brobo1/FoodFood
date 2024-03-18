ab4652f645ba7ea06a3378654552f47cffa2be03
Arne Ngo <arnengo2812@hotmail.com>
2024-03-18 10:47:29 +0100

    fixed some models

--- FoodFood/Data/Models/Personalia.cs
+++ FoodFood/Data/Models/Personalia.cs
@@ -7,7 +7,6 @@
         public string LastName { get; set; } = string.Empty;
         public DateTime Birthday { get; set; }
         public string Address { get; set; } = string.Empty;
-        public ICollection<Payment>? Payments { get; set; }
         public User? User { get; set; }
     }
 }
